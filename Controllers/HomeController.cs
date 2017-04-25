fed905fb6123eaace054e3ba4642e82b7f70e0f1
Tiago Casagrande Cundari <tccundari@gmail.com>
2017-04-25 14:27:43 -0300

    kill

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descrição resumida de HomeController
+/// </summary>
+public class HomeController
+{
+    public HomeController()
+    {
+        //
+        // TODO: Adicionar lógica do construtor aqui
+        //
+    }
+}
